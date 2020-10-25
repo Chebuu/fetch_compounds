ea0800b443cd3809e0949808c9657b33cbba7562
Chebuu <sethcbau@gmail.com>
2020-10-25 03:22:00 -0600

    increased default batch size to 100

--- fetch_compounds.py
+++ fetch_compounds.py
@@ -1,5 +1,5 @@
 import pubchempy as pc
-def fetch_compounds(cids, batch=50):
+def fetch_compounds(cids, batch=100):
     def gen(itr):
         for i in range(0,len(itr),batch):
             yield itr[i:min(i+batch,len(itr))]
