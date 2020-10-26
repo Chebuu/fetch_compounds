b803606d4864a84a1ad3c64ac50e97bb31365ec4
Chebuu <49116863+Chebuu@users.noreply.github.com>
2020-10-26 01:12:52 -0600

    coerce argument to list

--- fetch_compounds.py
+++ fetch_compounds.py
@@ -3,4 +3,4 @@ def fetch_compounds(cids, batch=100):
     def gen(itr):
         for i in range(0,len(itr),batch):
             yield itr[i:min(i+batch,len(itr))]
-    return sum([pc.get_compounds(q) for q in gen(cids)],[])
+    return sum([pc.get_compounds(q) for q in gen(list(cids))],[])
